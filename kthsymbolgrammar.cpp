b68ff3c57eaab32de5ef6e7822ae468280c361bf
Kathan Vakharia <kathankv5201@gmail.com>
2026-04-26 13:17:10 +0530

    added code

---
+++ kthsymbolgrammar.cpp
@@ -0,0 +1,12 @@
+class Solution {
+ public:
+	int kthGrammar(int n, int k) {
+		if (n == 1) return 0;
+
+		int entries = 1 << (n - 1);
+		if (k <= entries / 2)
+			return kthGrammar(n - 1, k);
+		else
+			return !kthGrammar(n - 1, k - entries / 2);
+	}
+};
